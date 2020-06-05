d48a57babf4aa75d173fe2991dbf614f1e411eb7
ArHaNiOl <arhaniol@op.pl>
2020-06-05 18:57:49 +0200

    coding

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,38 @@ public class App
     public static void main( String[] args ){
         Scanner scanner =new Scanner(System.in);
         int lines=Integer.parseInt(scanner.nextLine());
+        class Parameters{
+            int len;
+            int missing;
+            int[] sequence;
+
+            public Parameters(int missing, int[] sequence) {
+                this.len = sequence.length;
+                this.missing = missing;
+                this.sequence = sequence;
+            }
+            int computeNext(int len){
+                int dif=0;
+                for (int i = 0; i <len ; i++) {
+                    dif+=computeNext(len-1);
+                    if (dif > 0) {
+
+                    }
+                }
+                return dif;
+            }
+        }
+
+        Parameters[] parameters=new Parameters[lines];
         for (int i = 0; i < lines ; i++) {
             int len=scanner.nextInt(),
-                    missing=scanner.nextInt()
+                    missing=scanner.nextInt();
+            int[] sequence=new int[len];
+            for (int j = 0; j <len ; j++) {
+                sequence[i]=scanner.nextInt();
+                System.out.println(sequence[i]);
+            }
+            parameters[i]=new Parameters(missing,sequence);
         }
     }
 }
