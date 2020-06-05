79be8cf08be608f2ac413868e537ffe65e3d3ab2
arhaniol <arhaniol@op.pl>
2020-06-05 17:41:37 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        Scanner scanner =new Scanner(System.in);
+        int lines=Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < lines ; i++) {
+            int len=scanner.nextInt(),
+                    missing=scanner.nextInt()
+        }
+    }
+}
