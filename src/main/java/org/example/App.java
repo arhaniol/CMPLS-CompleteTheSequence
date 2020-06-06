c9925aa48ceea464fd1c0959e49b1b3fa09995a0
ArHaNiOl <arhaniol@op.pl>
2020-06-06 10:58:28 +0200

    Improving code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,48 +1,72 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ){
-        Scanner scanner =new Scanner(System.in);
-        int lines=Integer.parseInt(scanner.nextLine());
-        class Parameters{
-            int len;
-            int missing;
-            int[] sequence;
+public class App {
+    final int missing;
+    List<Integer> sequence;
+    List<Integer> temp;
 
-            public Parameters(int missing, int[] sequence) {
-                this.len = sequence.length;
-                this.missing = missing;
-                this.sequence = sequence;
-            }
-            int computeNext(int len){
-                int dif=0;
-                for (int i = 0; i <len ; i++) {
-                    dif+=computeNext(len-1);
-                    if (dif > 0) {
+    public App(int missing, List<Integer> sequence) {
+        this.missing = missing;
+        this.sequence = sequence;
+    }
 
-                    }
-                }
-                return dif;
+    void printNext() {
+        int last;
+        for (int i = 0; i < missing; i++) {
+            temp = new ArrayList<>(sequence);
+            last = computeNext(sequence.size());
+            sequence.add(last);
+            System.out.print(last);
+            if (i + 1 < missing) {
+                System.out.print(" ");
             }
         }
+    }
 
-        Parameters[] parameters=new Parameters[lines];
-        for (int i = 0; i < lines ; i++) {
-            int len=scanner.nextInt(),
-                    missing=scanner.nextInt();
-            int[] sequence=new int[len];
-            for (int j = 0; j <len ; j++) {
-                sequence[i]=scanner.nextInt();
-                System.out.println(sequence[i]);
+    int computeNext(int len) {
+        int dif = temp.get(len - 1);
+
+        if (len == 1) {
+            return temp.get(len - 1);
+        }
+
+        for (int i = 0; i < len - 1; i++) {
+            temp.set(i, temp.get(i + 1) - temp.get(i));
+        }
+        if (temp.get(len - 1) == 0) {
+            return temp.get(len - 1);
+        } else {
+            return dif + computeNext(len - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lines = Integer.parseInt(scanner.nextLine());
+
+        App[] parameters = new App[lines];
+        for (int i = 0; i < lines; i++) {
+            String[] line = scanner.nextLine().split(" ");
+            int len = Integer.parseInt(line[0]),
+                    missing = Integer.parseInt(line[1]);
+
+            List<Integer> sequence = new ArrayList<>();
+            for (int j = 0; j < len; j++) {
+                sequence.add(scanner.nextInt());
             }
-            parameters[i]=new Parameters(missing,sequence);
+            scanner.nextLine();
+            parameters[i] = new App(missing, sequence);
+        }
+
+        for (int i = 0; i < lines; i++) {
+            parameters[i].printNext();
+            if (i + 1 < lines) {
+                System.out.println();
+            }
         }
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,63 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void PrintTest_7_8_9() {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int[] outputList = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        App param = new App(3, inputList);
+        param.printNext();
+        assertArrayEquals(outputList, param.sequence.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    public void PrintTest_37_46() {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1,2,4,7,11,16,22,29));
+        int[] outputList = new int[]{1,2,4,7,11,16,22,29,37,46};
+
+        App param = new App(2, inputList);
+        param.printNext();
+        assertArrayEquals(outputList, param.sequence.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    public void PrintTest_11_56() {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1,1,1,1,1,1,1,1,1,2));
+        int[] outputList = new int[]{1,1,1,1,1,1,1,1,1,2,11,56};
+
+        App param = new App(2, inputList);
+        param.printNext();
+        assertArrayEquals(outputList, param.sequence.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    public void PrintTest_3() {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(3));
+        int[] outputList = new int[]{3,3,3,3,3,3,3,3,3,3,3};
+
+        App param = new App(10, inputList);
+        param.printNext();
+        assertArrayEquals(outputList, param.sequence.stream().mapToInt(i -> i).toArray());
     }
 }
